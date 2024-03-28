c08a0fd0652312fea9aed32b7205c1cd26b45212
2Yeonghoon <djaakclsrn12@naver.com>
2024-03-28 23:42:50 +0900

    CLN: 동일한 조건 묶어주기

--- index.ts
+++ index.ts
@@ -139,11 +139,12 @@ function handlerInputs() {
 function updateTile() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x].isStony && map[y + 1][x].isAir) {
-        map[y + 1][x] = new Stone(new Falling());
-        map[y][x] = new Air();
-      } else if (map[y][x].isBoxy && map[y + 1][x].isAir) {
-        map[y + 1][x] = new Box();
+      if (
+        (map[y][x].isStony && map[y + 1][x].isAir) ||
+        (map[y][x].isBoxy && map[y + 1][x].isAir)
+      ) {
+        map[y][x].drop();
+        map[y + 1][x] = map[y][x];
         map[y][x] = new Air();
       } else if (map[y][x].isFallingStone || map[y][x].isFallingBox) {
         map[y][x].rest();
